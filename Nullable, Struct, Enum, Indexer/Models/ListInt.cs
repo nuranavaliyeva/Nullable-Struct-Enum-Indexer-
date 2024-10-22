838585267b77f7e9dedd4a9fb3ba657ac56706ec
nuranavaliyeva <nuranavvaliyeva@gmail.com>
2024-10-22 11:35:38 +0400

    first commit

---
+++ Nullable, Struct, Enum, Indexer/Models/ListInt.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable__Struct__Enum__Indexer.Models
+{
+    internal class ListInt
+    {
+        private int[] arr;
+
+        public ListInt()
+        {
+            arr = new int[0];
+        }
+        public ListInt(int length)
+
+        {
+            arr = new int[length];
+        }
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= arr.Length)
+                {
+                    return arr[arr.Length - 1];
+                }
+                return arr[index];
+
+            }
+            set
+            {
+                if (index <= arr.Length)
+                {
+                    arr[index] = value;
+                }
+
+            }
+        }
+        public ListInt(params int[] nums)
+        {
+            arr = nums;
+        }
+        public int Length
+        {
+            get { return arr.Length; }
+        }
+        public void Add(int num)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = num;
+            Console.WriteLine($"{num} added");
+        }
+
+        public bool Contains(int num)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                {
+                    Console.WriteLine($"{num} var");
+                    return true;
+                  
+                }
+            }
+            Console.WriteLine($"{num} yoxdu");
+            return false;
+        }
+        public void Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine($" Sum: {sum}");
+        }
+        public void Remove(int num)
+        {
+            {
+                int j = 0;
+                int[] newArr = new int[arr.Length - 1];
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] != num)
+                    {
+                        newArr[j] = arr[i];
+                        j++;
+                    }
+                    
+                }
+                arr = newArr;
+                Console.WriteLine($"{num} this num is removed.");
+            }
+
+        }
+        public void AddRange(params int[] nums)
+        {
+            
+            int oldArrLength = arr.Length;
+            Array.Resize(ref arr, arr.Length + nums.Length);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr[oldArrLength + i] = nums[i];
+            }
+            Console.WriteLine("nums are added.");
+
+        }
+
+       
+        
+        public override string ToString()
+        {
+            
+            string result = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result += arr[i];
+                if (i < arr.Length - 1)
+                {
+                    result += ", "; 
+                }
+            }
+            Console.WriteLine(result);
+            return result;
+            
+        }
+    }
+}
---
+++ Nullable, Struct, Enum, Indexer/Program.cs
@@ -0,0 +1,22 @@
+﻿using Nullable__Struct__Enum__Indexer.Models;
+
+namespace Nullable__Struct__Enum__Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            ListInt arr1= new ListInt(9,6,3,9,10,23,2,2005);
+            arr1.Add(1);
+            arr1.Remove(1);
+            arr1.Contains(10);
+            arr1.Sum();
+            arr1.AddRange(20, 30, 40);
+            arr1.ToString();
+            arr1.ToString();
+
+        }
+   
+    }
+}
